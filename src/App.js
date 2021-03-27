6b4957bac1bc05f51758985f8238dc8257253fa6
Juan Buitrago <juanbuitrago2002@outlook.com>
2021-03-27 11:01:32 -0500

    pagian about us

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import gato from "./images/gato.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         pagina para conquistar <code>el amor de </code> juan bui
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="section-midle">
+        <div className="container">
+          <div className="container-text-about-us">
+            <div className="container-title">
+              <h1 className="title-number">01.</h1>
+              <h1 className="title">About us</h1>
+            </div>
+            <div className="container-text">
+              <p>
+                Hello! My name is Brittany and I enjoy creating things that live
+                on the internet. My interest in web development started back in
+                2012 when I decided to try editing custom.
+              </p>
+              <p>
+                Fast-forward to today, and I've had the privilege of working at
+                an advertising agency, a start-up, a huge corporation, and a
+                student-led design studio.
+              </p>
+              <p>
+                Here are a few technologies I've been working with recently:
+              </p>
+              <ul>
+                <li>JavaScript (ES6+)</li>
+                <li>Node.js</li>
+                <li>React</li>
+                <li>Vue</li>
+                <li>WordPress</li>
+              </ul>
+            </div>
+          </div>
+          <div className="container-img">
+            <img className="imagen-about-us" src={gato} alt="" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
