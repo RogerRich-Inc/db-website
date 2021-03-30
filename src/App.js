51608749ded06a4d1d1a323e506f368a36c2e701
Juan Buitrago <juanbuitrago2002@outlook.com>
2021-03-30 16:10:41 -0500

    terminado estilos

--- src/App.js
+++ src/App.js
@@ -42,14 +42,15 @@ function App() {
         </div>
         <div className="container container-projects">
           <div className="container-title title-projects">
-            <h1 className="title-number title-grande">02.</h1>
+            <h1 className="title-number-1 title-grande">0</h1>
+            <h1 className="title-number-2 title-grande">2.</h1>
             <h1 className="title title-prueba">Some Thing We Have Built</h1>
           </div>
           {/* <div className="space"></div> */}
           <div className="container-description">
             <h3 className="secondary-title">Une Sélection de</h3>
             <h4 className="third-title">21 projets disséqués</h4>
-            <p className="temporada">
+            <p className="text-projects">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Voluptates, voluptate? Quos corporis rerum delectus atque alias
               similique saepe amet eos fugit. Lorem ipsum dolor size.
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,26 +4,49 @@ import gato from "../images/gato.jpg";
 import "./styles/projects.css";
 function Projects() {
   return (
-    <div className="container-things-we">
-      <div className="container-img">
-        <img className="imagen-about-us" src={gato} alt="" />
-      </div>
+    <>
+      <div className="pru">
+        <div className="container-things-we">
+          <div className="container-img">
+            <img className="imagen-about-us" src={gato} alt="" />
+          </div>
 
-      <div className="container-text">
-        <div className="container-titlee">
-          <p className="title-project">Featured Project</p>
-          <h4 className="description-prajects">OctoProfile</h4>
-        </div>
+          <div className="container-text">
+            <div className="container-titlee">
+              <p className="title-project">Featured Project</p>
+              <h4 className="description-prajects">OctoProfile</h4>
+            </div>
 
-        <div className="container-text-projects">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-            quisquam ab numquam tempora amet eaque. Unde expedita quos sint nisi
-            laborum natus, odit at libero ut dolorem sequi id corporis.
-          </p>
+            <div className="container-text-projects">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                quisquam ab numquam tempora amet eaque. Unde expedita quos sint
+                nisi laborum natus, odit at libero ut dolorem sequi id corporis.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="container-things-we">
+          <div className="container-text">
+            <div className="container-titlee container-titlee-reverse">
+              <p className="title-project">Featured Project</p>
+              <h4 className="description-prajects">OctoProfile</h4>
+            </div>
+
+            <div className="container-text-projects container-text-projects-reverse">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                quisquam ab numquam tempora amet eaque. Unde expedita quos sint
+                nisi laborum natus, odit at libero ut dolorem sequi id corporis.
+              </p>
+            </div>
+          </div>
+          <div className="container-img container-img-reverse">
+            <img className="imagen-about-us" src={gato} alt="" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
