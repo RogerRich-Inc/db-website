f955b3da1dd572fbc0a7ca5c4ed363ad44040fc0
Juan Buitrago <juanbuitrago2002@outlook.com>
2021-04-13 16:40:54 -0500

    estilos

--- src/App.js
+++ src/App.js
@@ -28,11 +28,11 @@ function App() {
                 Here are a few technologies I've been working with recently:
               </p>
               <ul>
-                <li>JavaScript (ES6+)</li>
-                <li>Node.js</li>
-                <li>React</li>
-                <li>Vue</li>
-                <li>WordPress</li>
+                <li className="li1">JavaScript (ES6+)</li>
+                <li className="li2">Node.js</li>
+                <li className="li3">React</li>
+                <li className="li4">Vue</li>
+                <li className="li5">WordPress</li>
               </ul>
             </div>
           </div>
@@ -40,22 +40,7 @@ function App() {
             <img className="imagen-about-us" src={gato} alt="" />
           </div>
         </div>
-        <div className="container container-projects">
-          <div className="container-title title-projects">
-            <h1 className="title-number">02.</h1>
-            <h1 className="title">Some Thing We Have Built</h1>
-          </div>
-          {/* <div className="space"></div> */}
-          <div className="container-description">
-            <h3 className="secondary-title">Une Sélection de</h3>
-            <h4 className="third-title">21 projets disséqués</h4>
-            <p className="text-projects">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptates, voluptate? Quos corporis rerum delectus atque alias
-              similique saepe amet eos fugit. Lorem ipsum dolor size.
-            </p>
-          </div>
-        </div>
+
         <Project />
       </div>
     </>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,46 +5,66 @@ import "./styles/projects.css";
 function Projects() {
   return (
     <>
-      <div className="pru">
-        <div className="container-things-we">
-          <div className="container-img">
-            <img className="imagen-about-us" src={gato} alt="" />
-          </div>
-
-          <div className="container-text">
-            <div className="container-titlee">
-              <p className="title-project">Featured Project</p>
-              <h4 className="description-projects">OctoProfile</h4>
-            </div>
-
-            <div className="container-text-projects">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-                quisquam ab numquam tempora amet eaque. Unde expedita quos sint
-                nisi laborum natus, odit at libero ut dolorem sequi id corporis.
-              </p>
-            </div>
-          </div>
+      <div className="container container-projec">
+        <div className="container-title">
+          <h1 className="title-number">02.</h1>
+          <h1 className="title">Some Thing We Have Built</h1>
         </div>
-        <div className="container-things-we">
-          <div className="container-text">
-            <div className="container-titlee container-titlee-reverse">
-              <p className="title-project">Featured Project</p>
-              <h4 className="description-projects">OctoProfile</h4>
+        <div className="container-description">
+          <h3 className="secondary-title">Une Sélection de</h3>
+          <h4 className="third-title">21 projets disséqués</h4>
+          <p className="text-projects">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+            voluptate? Quos corporis rerum delectus atque alias similique saepe
+            amet eos fugit. Lorem ipsum dolor size.
+          </p>
+        </div>
+        <div className="container-projecs">
+          <div className="container-things-we">
+            <div className="container-img">
+              <img className="imagen-about-us" src={gato} alt="" />
             </div>
+
+            <div className="container-text">
+              <div className="container-titlee">
+                <p className="title-project">Featured Project</p>
+                <h4 className="description-projects">OctoProfile</h4>
+              </div>
+
+              <div className="container-text-projects">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                  quisquam ab numquam tempora amet eaque. Unde expedita quos
+                  sint nisi laborum natus, odit at libero ut dolorem sequi id
+                  corporis.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="container-things-we">
+            <div className="container-img container-img-hide">
+              <img className="imagen-about-us" src={gato} alt="" />
+            </div>
+
+            <div className="container-text">
+              <div className="container-titlee container-titlee-reverse">
+                <p className="title-project">Featured Project</p>
+                <h4 className="description-projects">OctoProfile</h4>
+              </div>
+              <div className="container-text-projects container-text-projects-reverse">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                  quisquam ab numquam tempora amet eaque. Unde expedita quos
+                  sint nisi laborum natus, odit at libero ut dolorem sequi id
+                  corporis.
+                </p>
+              </div>
+            </div>
+
             <div className="container-img container-img-reverse">
               <img className="imagen-about-us" src={gato} alt="" />
             </div>
-            <div className="container-text-projects container-text-projects-reverse">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-                quisquam ab numquam tempora amet eaque. Unde expedita quos sint
-                nisi laborum natus, odit at libero ut dolorem sequi id corporis.
-              </p>
-            </div>
-          </div>
-          <div className="container-img container-img-reverse">
-            <img className="imagen-about-us" src={gato} alt="" />
           </div>
         </div>
       </div>
