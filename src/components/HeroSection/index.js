30735d42e95fc1e74ed58fa165ae46257b55a62b
RogelioRichmanAstronaut <56854119+RogelioRichmanAstronaut@users.noreply.github.com>
2021-04-25 16:36:41 -0500

    5t421t54132451213r42
    God loves u

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -17,7 +17,7 @@ function HeroSection() {
     return (
         <HeroContainer>
             <HeroBg>
-            <VideoFondo src="https://player.vimeo.com/video/524797150?autoplay=1&loop=1&title=0&byline=0&portrait=0&muted=true&dnt=true&playsinline=true&quality=auto"
+            <VideoFondo src="https://player.vimeo.com/video/524797150?autoplay=1&loop=1&title=0&byline=0&portrait=0&muted=true&dnt=true&quality=auto"
                 frameborder="0"
                 allowfullscreen
                 allow="autoplay"
